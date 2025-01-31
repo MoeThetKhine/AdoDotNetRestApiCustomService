932f8fba4b31982984f24ec6473902e66514a1a7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 20:44:43 +0630

    Feat : Add Connection String

--- AdoDotNetRestApiCustomService/AdoDotNetService.cs
+++ AdoDotNetRestApiCustomService/AdoDotNetService.cs
@@ -1,6 +1,22 @@
-﻿namespace AdoDotNetRestApiCustomService
+﻿using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace AdoDotNetRestApiCustomService
 {
 	public class AdoDotNetService
 	{
+		private readonly string _connStr = "Data Source=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;";
+
+		private SqlConnection GetConnection() => new(_connStr);
+
+		
+
 	}
 }
+
+
+
+
+
+	
