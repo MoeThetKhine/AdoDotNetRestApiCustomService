343cf2f1b31ec3de42847ac8d9f6c2c42ffc11a7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 20:57:48 +0630

    Feat : Add CreateBlogQuery

--- AdoDotNetRestApiCustomService/Query.cs
+++ AdoDotNetRestApiCustomService/Query.cs
@@ -10,6 +10,8 @@
       ,[DeleteFlag]
   FROM [dbo].[Tbl_Blog] ORDER BY BlogId DESC";
 
+		public static string CreateBlogQuery { get; } =
+		"INSERT INTO dbo.Tbl_Blog (BlogTitle, BlogAuthor, BlogContent, DeleteFlag) VALUES (@BlogTitle, @BlogAuthor, @BlogContent, @DeleteFlag);";
 
 	}
 }
