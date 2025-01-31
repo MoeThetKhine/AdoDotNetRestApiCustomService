fe4bb6bda94e84f2998fe20e95c5763022fdc741
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 20:55:45 +0630

    Feat : Add GetAllBlogsQuery

---
+++ AdoDotNetRestApiCustomService/Query.cs
@@ -0,0 +1,13 @@
+﻿namespace AdoDotNetRestApiCustomService
+{
+	public class Query
+	{
+		public static string GetAllBlogsQuery { get; } =
+		@"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] ORDER BY BlogId DESC";
+	}
+}
