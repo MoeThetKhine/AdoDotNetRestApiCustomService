026b353e4772f98d1fed23bcf5e669c9cf40893a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 21:17:30 +0630

    Feat : Add GetAllBlogs

--- AdoDotNetRestApiCustomService/Controllers/AdoDotNetController.cs
+++ AdoDotNetRestApiCustomService/Controllers/AdoDotNetController.cs
@@ -12,5 +12,20 @@ namespace AdoDotNetRestApiCustomService.Controllers
 		{
 			_adoDotNetService = adoDotNetService;
 		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllBlogs()
+		{
+			try
+			{
+				var blogs = await _adoDotNetService.QueryAsync<BlogModel>(Query.GetAllBlogsQuery);
+				return Ok(blogs);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"Internal Server Error: {ex.Message}");
+			}
+
+
 	}
 }
