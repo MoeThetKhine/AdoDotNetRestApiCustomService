2942ab33c1b46c0a70fa0af29b4b2b3dbbfe0b11
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 21:29:18 +0630

    Format Code

--- AdoDotNetRestApiCustomService/Controllers/AdoDotNetController.cs
+++ AdoDotNetRestApiCustomService/Controllers/AdoDotNetController.cs
@@ -1,29 +1,28 @@
-﻿namespace AdoDotNetRestApiCustomService.Controllers
+﻿namespace AdoDotNetRestApiCustomService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AdoDotNetController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class AdoDotNetController : ControllerBase
+	private readonly AdoDotNetService _adoDotNetService;
+
+	public AdoDotNetController(AdoDotNetService adoDotNetService)
 	{
-		private readonly AdoDotNetService _adoDotNetService;
+		_adoDotNetService = adoDotNetService;
+	}
 
-		public AdoDotNetController(AdoDotNetService adoDotNetService)
+	[HttpGet]
+	public async Task<IActionResult> GetAllBlogs()
+	{
+		try
 		{
-			_adoDotNetService = adoDotNetService;
+			var blogs = await _adoDotNetService.QueryAsync<BlogModel>(Query.GetAllBlogsQuery);
+			return Ok(blogs);
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Internal Server Error: {ex.Message}");
 		}
 
-		[HttpGet]
-		public async Task<IActionResult> GetAllBlogs()
-		{
-			try
-			{
-				var blogs = await _adoDotNetService.QueryAsync<BlogModel>(Query.GetAllBlogsQuery);
-				return Ok(blogs);
-			}
-			catch (Exception ex)
-			{
-				return StatusCode(500, $"Internal Server Error: {ex.Message}");
-			}
-
-		}
 	}
 }
