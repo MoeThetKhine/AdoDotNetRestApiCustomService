d9d7476395b082f2fbb3f457abad837edc4d1ca8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 22:30:45 +0630

    Feat : Add DeleteBlog

--- AdoDotNetRestApiCustomService/Controllers/AdoDotNetController.cs
+++ AdoDotNetRestApiCustomService/Controllers/AdoDotNetController.cs
@@ -127,6 +127,23 @@ public class AdoDotNetController : ControllerBase
 
 	#endregion
 
+	[HttpDelete("{id}")]
+	public async Task<IActionResult> DeleteBlog(int id)
+	{
+		try
+		{
+			var parameters = new List<SqlParameter>
+			{
+				new("@BlogId", id)
+			};
 
+			int result = await _adoDotNetService.ExecuteAsync(Query.DeleteBlogQuery, parameters.ToArray());
+			return result > 0 ? Ok("Blog marked as deleted successfully.") : NotFound("Blog not found.");
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, $"Internal server error: {ex.Message}");
+		}
+	}
 
 }
